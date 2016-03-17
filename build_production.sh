3173436c68ca6dea2b403f887c5f2db551635c16
sirorezka <sirorezka@gmail.com>
2016-03-18 02:59:41 +0300

    aa

--- build_production.sh
+++ build_production.sh
@@ -12,4 +12,4 @@ rm -rf Data/trainSubReversedGraph_bin_map/
 rm -rf Data_short/commonFriendsCountsPartitioned_bin_map
 
 
-spark-1.6.0/bin/spark-submit --class "Baseline_full_v1" --master local[8] --driver-memory 24G My_Model_scala/target/scala-2.10/baseline_2.10-1.0.jar Data/
+spark-1.6.0/bin/spark-submit --class "Baseline_full_v1" --master local[*] --driver-memory 24G My_Model_scala/target/scala-2.10/baseline_2.10-1.0.jar Data/
